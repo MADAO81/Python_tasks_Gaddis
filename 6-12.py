6a4372cd59c86b8df32d905881fda32da1d8e993
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-03-17 15:27:04 +0300

    Add files via upload

---
+++ 6-12.py
@@ -0,0 +1,24 @@
+# The average number of steps. write a program that reads a file and outputs the average number of steps for each month, 
+# for 28 days in February.
+
+def main():
+    # make a tuple with months
+    mdays = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    # make a print form 
+    print("Month"+"\t\t"+"Average steps")
+    # open file to read
+    steps_file = open("steps.txt","r")
+    # get first date from file 
+    date = steps_file.readline()
+    start = 0 
+    for month in range(len(mdays)):
+        end = start + mdays[month]
+        steps = readline[begin:end]
+    sum_of_steps = 0 
+    for s in steps: 
+        sum_of_steps = sum_of_steps + int(s)
+        
+        
+    average = sum_of_steps/mdays[m]
+    print(str(m+1)+"\t\t"+str(average))
+    start = start +mdays[m]
