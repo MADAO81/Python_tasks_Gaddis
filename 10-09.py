e7bd140fc767cd0ee971809e7df6fc0a4318cc7f
Black Dow <Atyz@7324643>
2025-05-23 20:26:54 +0300

    unlucky_day

--- 10-09.py
+++ 10-09.py
@@ -1,67 +1,113 @@
-# task 10-09
+# Упражнение по программированию 10-9
 
-# Quiz
+# Викторина
 
-# import question  
-from objects import question # the class is stored in the objects folder
+# import question
+from objects import question  # класс хранится в папке objects
+
 
 def main():
-    # local valuables 
-    first_points = 0 
-    second_points = 0 
-    player = ""
+    # Локальные переменные
+    first_points = 0
+    second_points = 0
+    player = ''
 
-    # create question list
+    # Создать список вопросов.
     questions = get_questions()
-    
+
     for i in range(10):
-        
+
         if i % 2 == 0:
-            player = "One"
+            player = 'Один'
         else:
-            player = "Two"
-            
-        print("Question for player ", player, ":")
-        
+            player = 'Два'
+        print('Вопрос для игрока ', player, ':')
+
         current = questions[i]
         print(current)
-        user_answer = int(input("Enter your solution (number between 1 and 4): "))
+        user_answer = int(input('Введите ваше решение (номер' + \
+                                ' между 1 и 4): '))
         if current.isCorrect(user_answer):
-            if player == "One":
-                first_points +=1 
+            if player == 'Один':
+                first_points += 1
             else:
-                second_points +=1
-            print("This answer is correct!")
+                second_points += 1
+            print('Это правильный ответ.')
             print()
         else:
-            print("Incorrect. Correct answer is ", current.get_solution())
+            print('Неправильно. Правильный отет', \
+                  current.get_solution())
             print()
-            
-    print("First player gain", first_points, "points.")
-    print("Second player gain", second_points, "points.")
+
+    print('Первый игрок заработал', first_points, 'очков.')
+    print('Второй игрок заработал', second_points, 'очков.')
     if first_points == second_points:
-        print("Draw.")
+        print('Ничья.')
     elif first_points > second_points:
-        print("Player 1 wins.")
+        print('Первый игрок побеждает в игре.')
     else:
-        print("Player 2 wins.")
-      
+        print('Второй игрок побеждает в игре.')
+
+
 def get_questions():
-    
     questions = []
-    
-    # Create a dictionary of questions and add it to the list.
-    question2 = question.Question("How many days are there in a lunar year?", "354", "365","243","379",1)
+
+    # Создать словарь вопросов и добавить в список.
+    question1 = question.Question('Сколько дней в лунном ' + \
+                                  'году?', '354', '365', \
+                                  '243', '379', 1)
     questions.append(question1)
-    question2 = question.Question("What is the largest planet?", "Mars", "Jupiter", "Earth", "Pluto", 2)
+    question2 = question.Question('Какая самая большая планета?', \
+                                  'Марс', 'Юпитер', 'Земля', \
+                                  'Плутон', 2)
     questions.append(question2)
-    question3 = question.Question("Which whale is the biggest?", "Killer whale", "The Humpback Whale", "Beluga", "The Blue Whale", 4)
+    question3 = question.Question('Какой кит самый ' + \
+                                  'большой?', 'Косатка', \
+                                  'Горбатый кит', \
+                                  'Белуга', 'Синий кит', 4)
     questions.append(question3)
-    question4 = question.Question("What kind of dinosaur could fly?", "Triceratops", "Tyrannosaurus Rex", "Pteranodon", "Diplodocus", 3)
+    question4 = question.Question('Какой динозавр мог летать?', \
+                                  'Трицератопс', 'Тираннозавр', \
+                                  'Птеранодон', 'Диплодок', 3)
     questions.append(question4)
-    question5 = question.Question("Which of the penguins is the largest?", "Antarctic", "Golden-haired", "Emperor","Whitewing", 3)
-    
-    return questions 
-    
-# call main function 
+    question5 = question.Question('Какой из этих героев книги ' + \
+                                  'о Винни Пухе является осликом?', \
+                                  'Пух', 'Иа-Иа', 'Пятачок', \
+                                  'Канга', 2)
+    questions.append(question5)
+    question6 = question.Question('Какая из них самая жаркая планета?', \
+                                  'Марс', 'Плутон', 'Земля', \
+                                  'Венера', 4)
+    questions.append(question6)
+    question7 = question.Question('У какого динозавра был самый ' + \
+                                  'большой мозг по сравнению с ' + \
+                                  ' телом тела?', 'Троодон', \
+                                  'Стегозавр', \
+                                  'Ихтиозавр', 'Гигантораптор', 1)
+    questions.append(question7)
+    question8 = question.Question('Какой из пингвинов самый ' + \
+                                  'крупный?', \
+                                  'Антарктический пингвин', \
+                                  'Золотоволосый пингвин', \
+                                  'Императорский пингвин', \
+                                  'Белокрылый пингвин', 3)
+    questions.append(question8)
+    question9 = question.Question("В какой сказке героем " + \
+                                  'является обезъянка?', \
+                                  'Винни Пух', \
+                                  'Любопытный Джордж', 'Хортон', \
+                                  'Гуфи', 2)
+    questions.append(question9)
+    question10 = question.Question('Сколько длится год на Марсе?', \
+                                   '550 земных дней', \
+                                   '498 земных дней', \
+                                   '126 земных дней', \
+                                   '687 земных дней', 4)
+    questions.append(question10)
+
+    return questions
+
+
+# Вызвать главную функцию.
 main()
+
--- objects/cashRegister.py
+++ objects/cashRegister.py
@@ -1,39 +1,39 @@
-# CashRegister 
+# CashRegister
 
 import retail
 
 class CashRegister:
-    # initialize empty dictionary for purchased goods 
+    # initialize empty dictionary for purchased goods
     def __init__(self):
-        
+
         self.__items = []
-        
+
     # Cleans the contents of the cash register.
     def clear(self):
-        
+
         self.__items = []
-        
+
     # Simulates adding an item to the cash register. Gets the Retail Item object as an argument.
     def purchase_item(self, retail_item):
-        
+
         self.__items.append(retail_item)
         print("The product was added to the cash register.")
-        
+
     # Returns the total cost of the items at the cash register.
     def get_total(self):
-        
+
         total = 0.0
         for item in self.__items:
             total = total + item.get_price()
-            
-        return total 
-        
+
+        return total
+
     # Prints the list of products in the cash register.
     def show_items(self):
-        
+
         print("Products at the cash register: ")
         print()
         for item in self.__items:
             print(item)
             print()
-        
+
