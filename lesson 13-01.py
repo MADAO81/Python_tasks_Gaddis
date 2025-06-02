a4c56bb4f8f0262721d565fa598cd5b1feff922c
Black Dow <Atyz@7324643>
2025-06-02 20:01:39 +0300

    interface_practice_start

---
+++ lesson 13-01.py
@@ -0,0 +1,13 @@
+# this program shows empty window
+
+import  tkinter
+
+def main():
+    # create interface element of main window
+    main_window = tkinter.Tk()
+
+    # enter to the main loop tkinter
+    tkinter.mainloop()
+
+# call main function
+main()
