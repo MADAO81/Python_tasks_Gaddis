8235df521ec688b7689145ff190a633a02b73e82
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-06-03 16:30:51 +0300

    Add files via upload

---
+++ lesson 13-07.py
@@ -0,0 +1,30 @@
+# This program demonstrates the Button interface element.
+# When the user clicks the Button, an information dialog box is displayed on the screen.
+
+import tkinter
+import tkinter.messagebox 
+
+class MyGUI:
+    def __init__(self):
+        # Create an interface element for the main window.
+        self.main_window = tkinter.Tk()
+        
+        # Create a Button widget interface element. 
+        # The text 'Click me!' should appear on the button.
+        # When the user clicks the button,
+        # the do_something method should be executed.
+        self.my_button = tkinter.Button(self.main_window, text = "Click me!", command = self.do_something)
+        
+        # Package the Button interface element.
+        self.my_button.pack()
+        
+        # Enter the main tkinter loop.
+        tkinter.loop()
+        
+    # The do something method is a callback function for the Button interface element.
+    def do_something(self):
+        # Show the information dialog box
+        tkinter.messagebox.showinfo("Reaction", "Praise you for push the button!")
+        
+# Create an instance of MyGUI class 
+my_gui = MyGUI()
