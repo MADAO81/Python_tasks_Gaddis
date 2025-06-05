864c4dcde99710aa9217fae6d3220015813f514c
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-06-05 15:33:23 +0300

    Add files via upload

---
+++ lesson 13-16.py
@@ -0,0 +1,22 @@
+# This program draws a rectangle on the Canvas element.
+import tkinter
+
+class MyGUI:
+    def __init__(self):
+        # create main window 
+        self.main_window = tkinter.Tk()
+        
+        # create the Canvas interface element.
+        self.canvas = tkinter.Canvas(self.main_window, width = 200, height = 200)
+        
+        # Draw rectangle 
+        self.canvas.create_rectangle(20, 20, 180, 180)
+        
+        # pack canvas 
+        self.canvas.pack()
+        
+        # start main loop
+        tkinter.mainloop()
+        
+# create an instance of MyGUI class 
+my_gui = MyGUI()
