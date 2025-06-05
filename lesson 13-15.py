c63dfb546b7e8bdf727bf9e177a300454da51d23
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-06-05 15:21:10 +0300

    Add files via upload

---
+++ lesson 13-15.py
@@ -0,0 +1,22 @@
+# This program connects several points with straight line segments.
+import tkinter
+
+class MyGUI:
+    def __init__(self):
+        # create main window 
+        self.main_window = tkinter.Tk()
+        
+        # create the Canvas interface element.
+        self.canvas = tkinter.Canvas(self.main_window, width = 200, height = 200)
+        
+        # Draw straight line segments connecting several points.
+        self.canvas.create_line(10, 10, 189, 10, 100, 189, 10, 10)
+        
+        # pack canvas 
+        self.canvas.pack()
+        
+        # start main loop
+        tkinter.mainloop()
+        
+# create an instance of MyGUI class 
+my_gui = MyGUI()
