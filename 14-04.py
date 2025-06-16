3b211de9cacd8f7cf5f16263764344e4a5a4e260
Evgeniy Chernyshev <106910022+MADAO81@users.noreply.github.com>
2025-06-16 13:02:15 +0300

    Add files via upload

---
+++ 14-04.py
@@ -0,0 +1,30 @@
+# This program demonstrates 
+# the functional version of the range_sum function
+
+def main():
+    # Algorithm 
+    def range_sum(data):  
+        seq, params = data
+        fn = lambda start, end: 0 if start > end \
+                                  else seq[start] + fn(start + 1, end)
+        return fn(*params)  
+        
+    # data input
+    def indata():
+        seq = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        params = (2,5) # params are the start and end parameters.
+        return seq, params 
+        
+    # data output 
+    def outdata():
+        def f(data):
+            msg = "The sum of the values from position 2 to position 5 is "
+            print(msg, format(data), sep = "")
+        return f 
+
+    # Pipeline (functional core)
+    pipe(indata(), range_sum, outdata())
+    
+# call main function
+main()
+    
